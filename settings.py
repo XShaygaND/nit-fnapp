8d164e5a7dd05962080eace5cc69a60d779cc2e6
Shaygan Askari <shaygan.askari@gmail.com>
2023-11-20 01:27:44 +0330

    [fix] Fix dinner time.

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pytz import timezone
 ## GENERAL ##
 BREAKFAST_LIMIT = {'hour': 8, 'min': 0}
 LUNCH_LIMIT = {'hour': 13, 'min': 30}
-DINNER_LIMIT = {'hour': 23, 'min':45}
+DINNER_LIMIT = {'hour': 18, 'min':45}
 
 ## USER ##
 MAX_WARNS = 3
